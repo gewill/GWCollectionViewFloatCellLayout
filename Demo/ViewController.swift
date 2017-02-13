68762e1867f811c76267039868d5ffc9fccb7b6a
Ge Will <531sunlight@gmail.com>
2017-02-13 17:47:59 +0800

    update deomo: float header use default UICollectionViewFlowLayout

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,46 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
+
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var layout: UICollectionViewFlowLayout!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.automaticallyAdjustsScrollViewInsets = false
-        
+
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "scrollDirection", style: .plain, target: self, action: #selector(self.scrollDirectionButtonClick(_:)))
-        
-        
+
+
         collectionView.delegate = self
         collectionView.dataSource = self
-        
+
         self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        
+
         self.collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
         self.collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "footer")
-        
+
+        if #available(iOS 9.0, *) {
+            self.layout.sectionHeadersPinToVisibleBounds = true
+        } else {
+            // Fallback on earlier versions
+        }
+
     }
-    
+
     // MARK: - UICollectionViewDelegate
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 2
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        if section == 0 {
+            return 0
+        } else {
+            return 500
+        }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         cell.backgroundColor = .gray
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
+
         if kind == UICollectionElementKindSectionHeader {
             let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header", for: indexPath)
             if indexPath.section % 2 != 0 {
@@ -67,45 +77,53 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             return view
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+
     }
-    
+
     // MARK: - UICollectionViewDelegateFlowLayout
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
+
         return CGSize(width: 30, height: 30)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        
-        return UIEdgeInsets(top: 10, left: 20, bottom: 30, right: 40)
+        if section == 0 {
+            return .zero
+        } else {
+            return UIEdgeInsets(top: 10, left: 20, bottom: 30, right: 40)
+        }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        
+
         return 2
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        
+
         return 10
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        
-        return CGSize(width: 100, height: 30)
-        
+        if section == 0 {
+            return CGSize(width: 0, height: 0)
+        } else {
+            return CGSize(width: 100, height: 40)
+        }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        
-        return CGSize(width: 200, height: 22)
+        if section == 0 {
+            return CGSize(width: 0, height: 300)
+        } else {
+            return CGSize(width: 200, height: 22)
+        }
     }
-    
-    
+
+
     // MARK: - response methods
     func scrollDirectionButtonClick(_ sender: UIBarButtonItem) {
         if self.layout.scrollDirection == .horizontal {
@@ -114,7 +132,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             self.layout.scrollDirection = .horizontal
         }
     }
-    
-    
-    
+
+
+
 }
